cecd9602f38ac16aeacb5c3db47f283551300257
CaesarJeries <caesar.dakwar@outlook.co.il>
2021-03-27 15:03:55 +0300

    expanded the skeleton to include option handling. adding documentation

--- stock_analyzer.py
+++ stock_analyzer.py
@@ -2,8 +2,16 @@
 
 import yfinance
 
-# todo
+
+
+
 def get_user_choice():
+    """
+    Displays the menu, and prompts the user to enter their choice.
+
+    @return - the user's "choice path": the series of choices the user made
+              until an operation is chosen to be performed.
+    """
     menu = {
         'a': {
             'msg': 'Display stock data',
@@ -40,18 +48,55 @@ def get_user_choice():
             return choice_path
 
 
+def get_user_input():
+    """
+    Prompts the user to enter a stock name, a start and end dates.
+    It then parses the input, and returns three strings:
+        - The name of the selected stock
+        - The start date of the desired time period
+        - The end date of the desired time period
+    
+    Note: The dates are in the following format: dd/mm/yyyy
+    """
+    return None, None, None # todo
+
+
+def display_available_stocks(companies):
+    """
+    Displays the list of companies that the user can choose from
+    to read statistics about their stocks during a selected time period.
+
+    @return - this function does not return a value
+    """
+    print('Available stocks:')
+    for c in companies:
+        print(c)
+
+
+def display_stock_stats(company, start_date, end_date):
+    """
+    Displays statistics about the given `stock` in the time period that is selected by the user.
+    """
+    pass
+
+
 def main():
     companies = ['EBAY' , 'ecl', 'Eix', 'ew', 'ea']
 
     while True:
         choice = get_user_choice()
-        print('User\'s choice: {}'.format(choice))
+        print('User\'s choice: {}'.format(choice)) # todo: remove before submission
 
-        company = 'ea' # todo: get user input
-        if company not in companies:
-            print('The selected company "{}" is not among the available stock symbols'.format(company))
-            continue
-
+        if choice == 'b':
+            break
+        elif choice == 'aa':
+            display_available_stocks(companies)
+        elif choice == 'ab':
+            company, start_date, end_date = get_user_input()
+            if company not in companies:
+                print('The selected company "{}" is not among the available stock symbols'.format(company))
+                continue
+            display_stock_stats(company, start_date, end_date)
 
 
 if __name__ == '__main__':
